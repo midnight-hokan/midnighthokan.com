2455f55354770944e6e3baab17cea7c2b0f40774
midnight-hokan <kikalukiller2017@gmail.com>
2026-06-12 03:31:21 +0000

    feat: 指挥中心风格首页上线

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '深夜补完计划';
+export const SITE_DESCRIPTION = '当世界入睡，我们开始补完。AI 硬件玩家 / 本地大模型部署 / Maker / 内容创作者';
